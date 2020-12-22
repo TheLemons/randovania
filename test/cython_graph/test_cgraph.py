9494f08c64406559d36e77985c410d7cc9849008
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-12-22 13:09:37 +0200

    cgraph

--- test/cython_graph/test_cgraph.py
+++ test/cython_graph/test_cgraph.py
@@ -2,7 +2,7 @@ import collections
 import pprint
 from random import Random
 
-from randovania.cython_graph.cgraph import cgraph_from
+from randovania.cython_graph.cgraph import optimize_world
 from randovania.game_description.requirements import ResourceRequirement
 from randovania.game_description.resources.resource_type import ResourceType
 from randovania.generator import generator
@@ -17,8 +17,8 @@ def test_database_collectable(preset_manager):
     pool = generator.create_player_pool(Random(15000), preset_manager.default_preset.get_preset().configuration, 0)
     game, state = logic_bootstrap(pool.configuration, pool.game, pool.patches)
 
-    graph = cgraph_from(game.world_list, state.patches)
+    graph = optimize_world(game.world_list, state.patches, game.dangerous_resources)
 
-    pprint.pprint(adjacency)
+    pprint.pprint(graph)
 
     assert False
